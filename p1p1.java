e9e98dd6ee6366a870cbdef01ae963b9e21ddf2f
naylaakas <naylakas@gmail.com>
2024-10-31 10:30:17 +0700

    p1p1

---
+++ p1p1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ArrayBilangan21 {
+
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+        int[] bil = {5, 13, -7, 17};
+
+        for (int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
+    }
+}
